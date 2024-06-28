17d5be4b54e5a704a095a0e38f1fc2da867c597f
ksg-dev <ksg.dev.data@gmail.com>
2024-06-28 13:06:15 -0500

    format titles reverse order

--- main.py
+++ main.py
@@ -20,23 +20,19 @@ for title in titles:
     movies.append(movie)
 
 
-sorted_list = []
+title_list = []
 for movie_name in movies:
     try:
         movie_tups = movie_name.split(")")[1].strip()
     except IndexError:
         movie_tups = movie_name.split(":")[1].strip()
-    sorted_list.append(movie_tups)
-    print(movie_tups)
-
-print(sorted_list)
-
-
-# for movie in sorted_movies:
-#     with open("movies.txt", mode="w") as file:
-#         file.write(f"{movie}\n")
-
-# print(sorted_movies)
-# item = sorted_movies[0].split(")")
-# print(item)
-# print(final_list)
+    title_list.append(movie_tups)
+    # print(movie_tups)
+sorted_list = reversed(title_list)
+# print(sorted_list)
+start = 1
+with open("movies.txt", mode="w") as file:
+    for title_name in sorted_list:
+        formatted_title = f"{start}) {title_name}"
+        file.write(f"{formatted_title}")
+        start += 1
